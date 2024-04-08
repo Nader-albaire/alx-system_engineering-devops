9ca7bf0511eca6a941bc3a50a2d1eff4f20d5c98
Nader-albaire <nader.albaire@gmail.com>
2024-04-07 17:55:59 -0700

    New file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,16 @@
-file { '/etc/ssh/ssh_config':
-    ensure  => present,
-    content => "
-        # SSH client configuration
-        Host *
-            IdentityFile ~/.ssh/school
-            PasswordAuthentication no
-    ",
+# make changes to config file using Puppet
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
 
