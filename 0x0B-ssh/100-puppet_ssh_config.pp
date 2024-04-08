45da9413979d1d3e3558ebe286ec2b207e1257ac
Nader-albaire <nader.albaire@gmail.com>
2024-04-07 17:51:24 -0700

    New file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,10 @@
-#!/usr/bin/env bash
-# using puppet to make changes to the configuration file
-
-file { 'etc/ssh/ssh_config':
-	ensure => presnet,
-
-content => "
-
-	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	"
+file { '/etc/ssh/ssh_config':
+    ensure  => present,
+    content => "
+        # SSH client configuration
+        Host *
+            IdentityFile ~/.ssh/school
+            PasswordAuthentication no
+    ",
 }
+
